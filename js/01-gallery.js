8540f1f8a1f578492fe03a9b8ffc9207f197df36
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-09-25 22:56:31 +0300

    task 1  finished
    need help with closing modal window.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 // Change code below this line
 
-console.log(galleryItems);
+// ----------1-Creating Markup-------------
+
+const renderMarkupTemplate = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`;
+
+const imgContainer = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map((item) => renderMarkupTemplate(item))
+  .join(``);
+imgContainer.insertAdjacentHTML("afterbegin", galleryList);
+
+// -----------------------------------------------------------
+
+// ----------2 --- EventListener -----
+
+imgContainer.addEventListener(`click`, onImageHandleClick);
+
+let instanceModal = basicLightbox.create(`<div class="modal">
+      </div>`);
+instanceModal.show();
+
+// ---------EventListener for opening ------------
+function onImageHandleClick(evt) {
+  evt.preventDefault();
+
+  const isImgActive = evt.target.classList.contains("gallery__image");
+
+  if (!isImgActive) {
+    return;
+  }
+  const activeImg = evt.target;
+
+  let instanceModal = basicLightbox.create(`<div class="modal">
+    <img src = "${activeImg.dataset.source}"/> </div>`);
+  instanceModal.show();
+}
+// ---------EventListener for closing  with Escape on  keyboard-----
+// ------ need tutors help ----
+imgContainer.addEventListener(`keydown`, onEscCloseModal, { once: true });
+
+function onEscCloseModal(evt) {
+  if (evt.key === "Escape") {
+    console.log(`escape is pressed`);
+    console.log(instanceModal);
+    instanceModal.close();
+  }
+}
